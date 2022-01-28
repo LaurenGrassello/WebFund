f87e3b2158692e7dab49a9752d3b11164480873a
LaurenOdalen <laurenodalen@gmail.com>
2022-01-27 20:36:42 -0700

    loop challenges

---
+++ loopChallenges.js
@@ -0,0 +1,53 @@
+// print odds 1-20
+
+function printOdds1To20() {
+
+    for (var i = 1; i <= 20; i += 2) {
+        console.log(i);
+    }
+}
+printOdds1To20();
+
+// decrease multiples of 3
+
+for (var i = 100; i >=0; i --) {
+    if (i % 3 == 0){
+    console.log(i);
+    }
+}
+
+    
+// print the sequence
+
+var arr=[4, 2.5, 1, -0.5, -2, -3.5];
+
+for(i=0; i<arr.length; i++){
+    
+}
+console.log(arr);
+
+
+// sigma
+
+var sum = 0;
+function printSum() {
+    for(var i = 0; i<=100; i++){
+        sum = sum + i;
+    }
+    console.log(sum);
+
+}
+printSum();
+
+
+// Factorial
+
+var product = 1;
+function multiplySum() {
+    for(var i = 1; i<=12; i++){
+        product = product * i;
+    }
+    console.log(product);
+
+}
+multiplySum();
