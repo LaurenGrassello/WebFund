9a45974fc6b6739fa96e9b1581e392aaebde890b
LaurenOdalen <laurenodalen@gmail.com>
2022-02-01 17:36:50 -0700

    pp js

---
+++ likingLikes/pt2_script.js
@@ -0,0 +1,35 @@
+// first user
+
+var numLikes1 = 9;
+var likeElement1 = document.querySelector(".numLikes1");
+console.log(likeElement1);
+
+function addLikes1(){
+    numLikes1++;
+    likeElement1.innerText = numLikes1;
+    console.log(numLikes1);
+}
+
+// second user
+
+var numLikes2 = 12;
+var likeElement2 = document.querySelector(".numLikes2");
+console.log(likeElement2);
+
+function addLikes2(){
+    numLikes2++;
+    likeElement2.innerText = numLikes2;
+    console.log(numLikes2);
+}
+
+// third user
+
+var numLikes3 = 9;
+var likeElement3 = document.querySelector(".numLikes3");
+console.log(likeElement3);
+
+function addLikes3(){
+    numLikes3++;
+    likeElement3.innerText = numLikes3;
+    console.log(numLikes3);
+}
